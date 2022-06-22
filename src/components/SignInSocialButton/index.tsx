c9140ccb927c742166bd6a4f40ca6ccdb8d8d1d3
Key Yu Wan <keyflcbyuwan@gmail.com>
2022-06-22 10:44:48 -0300

    Sign in screen

---
+++ src/components/SignInSocialButton/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { RectButtonProps } from "react-native-gesture-handler";
+import { SvgProps } from "react-native-svg";
+
+import { Button, IconContainer, Title } from "./styles";
+
+interface SignInSocialButtonProps extends RectButtonProps {
+  title: string;
+  svg: React.FC<SvgProps>;
+}
+
+export function SignInSocialButton({
+  title,
+  svg: Svg,
+  ...rest
+}: SignInSocialButtonProps) {
+  return (
+    <Button {...rest}>
+      <IconContainer>
+        <Svg />
+      </IconContainer>
+
+      <Title>{title}</Title>
+    </Button>
+  );
+}
